13eaa6adfb8505c70a68166f4504a0e971cadfb4
Tomas_Bacigalupo <69203584+tomasbacigalupo-tintin@users.noreply.github.com>
2025-05-23 13:56:09 -0300

    Add Tauri UI skeleton

---
+++ ui/tauri/src-tauri/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
